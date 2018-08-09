d1df66a09030a0e8fe79baee6e9873a60f304d07
imoonstal <imoonstal@gmail.com>
2018-08-10 00:44:07 +0800

    增加part8

---
+++ part8-proof-of-work/BLC/Block.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part8_proof_of_work.BLC
+{
+    class Block
+    {
+        #region 区块结构
+        //1. 区块高度
+        public long Height { get; set; }
+        //2. 上一个区块HASH
+        public byte[] PrevBlockHash { get; set; }
+        //3. 交易数据
+        public byte[] Data { get; set; }
+        //4. 时间戳
+        public long Timestamp { get; set; }
+        //5. Hash
+        public byte[] Hash { get; set; }
+        //6.Nonce
+        public long Nonce { get; set; }
+        #endregion
+
+        //1.生成一个新区块
+        /// <summary>
+        /// 生成一个新区块
+        /// </summary>
+        /// <param name="data">交易数据</param>
+        /// <param name="height">区块的高度</param>
+        /// <param name="prevBlockHash">上一个区块的hash</param>
+        /// <returns></returns>
+        public Block(string data, long height, byte[] prevBlockHash)
+        {
+            //创建区块
+            Height = height;
+            PrevBlockHash = prevBlockHash;
+            Data = Encoding.Default.GetBytes(data);
+            Timestamp = Utils.GetTimeStamp();
+            Nonce = 0;
+            // 调用工作量证明的方法并且返回有效的Hash和Nonce
+            var pow = new ProofOfWork(this);
+            var (hash, nonce) = pow.Run();
+
+            this.Hash = hash;
+            this.Nonce = nonce;
+        }
+
+
+        //2. 单独写一个方法，生成创世区块
+        public Block(string data):this(data, 1, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
+        {
+        }
+
+
+        /// <summary>
+        /// 重写Block的ToString方法
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "{" + $"{this.Height} {Utils.PrintArray(this.PrevBlockHash)} {Utils.PrintArray(this.Data)} {this.Timestamp} {Utils.PrintArray(this.Hash)} {this.Nonce}" + "}";
+        }
+
+
+
+        //1.生成一个新区块
+        /// <summary>
+        /// 生成一个新区块
+        /// </summary>
+        /// <param name="data">交易数据</param>
+        /// <param name="height">区块的高度</param>
+        /// <param name="prevBlockHash">上一个区块的hash</param>
+        /// <returns></returns>
+        //public static Block NewBlock(string data, long height, byte[] prevBlockHash)
+        //{
+        //    //创建区块
+        //    var d = Encoding.Default.GetBytes(data);
+        //    var block = new Block { Height = height, PrevBlockHash = prevBlockHash, Data = d, Timestamp = Utils.GetTimeStamp(), Nonce = 0 };
+        //    // 调用工作量证明的方法并且返回有效的Hash和Nonce
+        //    var pow = new ProofOfWork(block);
+        //    var (hash, nonce) = pow.Run();
+
+        //    block.Hash = hash;
+        //    block.Nonce = nonce;
+
+        //    return block;
+        //}
+
+
+        //2. 单独写一个方法，生成创世区块
+        //public static Block CreateGenesisBlock(string data)
+        //{
+        //    return NewBlock(data, 1, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        //}
+    }
+}
---
+++ part8-proof-of-work/Program.cs
@@ -0,0 +1,40 @@
+﻿using part8_proof_of_work.BLC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part8_proof_of_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 创世区块
+            //var blockchain = Blockchain.CreateBlockchainWithGenesisBlock();
+
+            //// 新区块
+            //blockchain.AddBlockToBlockchain("Send 100RMB To zhangqiang", blockchain.Blocks[blockchain.Blocks.Count - 1].Height + 1, blockchain.Blocks[blockchain.Blocks.Count - 1].Hash);
+            //blockchain.AddBlockToBlockchain("Send 200RMB To changjingkong", blockchain.Blocks[blockchain.Blocks.Count - 1].Height + 1, blockchain.Blocks[blockchain.Blocks.Count - 1].Hash);
+            //blockchain.AddBlockToBlockchain("Send 300RMB To juncheng", blockchain.Blocks[blockchain.Blocks.Count - 1].Height + 1, blockchain.Blocks[blockchain.Blocks.Count - 1].Hash);
+            //blockchain.AddBlockToBlockchain("Send 50RMB To haolin", blockchain.Blocks[blockchain.Blocks.Count - 1].Height + 1, blockchain.Blocks[blockchain.Blocks.Count - 1].Hash);
+
+            //Console.WriteLine(blockchain);
+
+            ////fmt.Println(blockchain.Blocks)
+
+            var block = new Block("Test", 1, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            Console.WriteLine(block.Nonce);
+            Console.WriteLine(Utils.SHA256byteArr2String(block.Hash));
+
+            var proofOfWork = new ProofOfWork(block);
+            var b = proofOfWork.IsValid();
+            Console.WriteLine(b);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
