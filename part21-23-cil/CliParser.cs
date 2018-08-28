96db1409aa58eebd029bd8ee15aed20d187ed948
imoonstal <imoonstal@gmail.com>
2018-08-29 00:54:31 +0800

    增加一个命令行参数解析类CliParser

---
+++ part21-23-cil/CliParser.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part21_23_cil
+{
+    /// <summary>
+    /// 命令行参数解析类
+    /// 没找到好用的CommandLine的解析工具，自己动手随便写一个简单的解析工具类。
+    /// </summary>
+    class CliParser
+    {
+        //命令列表
+        static List<string> cmds = new List<string>();
+        //参数列表
+        static List<string> argList;
+        //key是命令名称，value是包含命令本身的所有参数
+        static Dictionary<string, List<string>> cmdObj = new Dictionary<string, List<string>>();
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("\taddblock -data DATA -- 交易数据.");
+            Console.WriteLine("\tprintchain -- 输出区块信息.");
+        }
+
+        public static void IsValidArgs(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                PrintUsage();
+                Environment.Exit(0);
+            }
+        }
+
+        /// <summary>
+        /// 初始化命令
+        /// </summary>
+        /// <param name="args"></param>
+        public static void InitCmd(string[] args)
+        {
+            argList = args.ToList();
+            var list = new List<CmdIndex>();
+            cmds.ForEach(cmd =>
+            {
+                var n = argList.IndexOf(cmd);
+                if (n != -1)
+                    list.Add(new CmdIndex { Cmd = cmd, Index = n });
+            });
+            list.Sort();
+            for (int i = 0; i < list.Count; i++)
+            {
+                List<string> subCmds;
+                if (i != list.Count - 1)
+                    subCmds = argList.Skip(list[i].Index).Take(list[i + 1].Index - list[i].Index).ToList();
+                else
+                    subCmds = argList.Skip(list[i].Index).Take(argList.Count - list[i].Index).ToList();
+                cmdObj.Add(list[i].Cmd, subCmds);
+            }
+        }
+        /// <summary>
+        /// 注册需要的命令
+        /// </summary>
+        /// <param name="cmd"></param>
+        public static void Register(string cmd)
+        {
+            if (!cmds.Contains(cmd))
+                cmds.Add(cmd);
+        }
+        /// <summary>
+        /// 反注册命令
+        /// </summary>
+        /// <param name="cmd"></param>
+        public static void UnRegister(string cmd)
+        {
+            if (cmds.Contains(cmd))
+                cmds.Remove(cmd);
+        }
+
+        public static object Parse(string cmd)
+        {
+            if (!cmdObj.ContainsKey(cmd))
+                return "";
+            switch (cmd)
+            {
+                case "addblock":
+                    return cmdObj[cmd][2];
+                case "printchain":
+                    return cmdObj[cmd][1];
+                default:
+                    return "";
+            }
+        }
+    }
+
+    class CmdIndex : IComparable
+    {
+        public string Cmd { get; set; }
+        public int Index { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            if (obj is CmdIndex other)
+                return Index.CompareTo(other.Index);
+            else
+                throw new ArgumentException("Object is not a CmdIndex");
+        }
+    }
+}
--- part21-23-cil/Program.cs
+++ part21-23-cil/Program.cs
@@ -10,7 +10,25 @@ namespace part21_23_cil
     {
         static void Main(string[] args)
         {
-            //没找到好用的CommandLine的解析工具，这个工程没内容，占坑
+            //args = new string[] { "addblock", "-data", "ddd", "printchain", "aaaa" };
+            CliParser.IsValidArgs(args);
+
+            CliParser.Register("addblock");
+            CliParser.Register("printchain");
+            CliParser.InitCmd(args);
+
+            if (!string.IsNullOrWhiteSpace(CliParser.Parse("addblock").ToString()))
+                Console.WriteLine(CliParser.Parse("addblock"));
+            if (!string.IsNullOrWhiteSpace(CliParser.Parse("printchain").ToString()))
+                Console.WriteLine(CliParser.Parse("printchain"));
+
+            //测试
+            //编译完，在命令行运行
+            //bc addblock -data  send100RMBtome
+            //结果
+            //send100RMBtome
+
         }
     }
+
 }
