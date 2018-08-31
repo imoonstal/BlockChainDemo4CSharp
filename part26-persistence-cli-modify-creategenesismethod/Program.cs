f5898993686319fb91446a858d2ce02d87acaf47
imoonstal <imoonstal@gmail.com>
2018-08-31 22:16:21 +0800

    Add part26

---
+++ part26-persistence-cli-modify-creategenesismethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using BLC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part26_persistence_cli_modify_creategenesismethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                var cli = new CLI();
+                cli.Run(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"运行异常:{ex.Message}");
+            }
+        }
+    }
+}
