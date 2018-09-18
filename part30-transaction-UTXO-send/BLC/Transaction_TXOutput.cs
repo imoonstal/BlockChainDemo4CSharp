a253b0ea65c8de5618a661c297b35f075689bdd7
imoonstal <imoonstal@gmail.com>
2018-09-18 23:36:41 +0800

    增加part30

---
+++ part30-transaction-UTXO-send/BLC/Transaction_TXOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLC
+{
+    [Serializable]
+    class TXOutput
+    {
+        //支出的钱的数量
+        public long Value { get; set; }
+        //钱给了谁，如果钱给出去有找零，那么找零的钱就给自己
+        public string ScriptPubKey { get; set; }
+    }
+}
